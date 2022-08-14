3a3b9feae0a08581c1c15b8898ef29b9012e5c95
Kostiantyn <cashalot80@ukr.net>
2022-08-14 21:53:17 +0100

    Add Button, Form

---
+++ src/components/Phonebook/Button/ButtonSubmit.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  margin-top: 20 px;
+  weight: 100px;
+  height: 30px;
+  border: 3px solid #0505ec;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+export default Button;
